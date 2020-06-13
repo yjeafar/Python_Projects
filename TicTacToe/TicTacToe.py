d2fcbf6a0f655d000dfde2205828593b229c8341
Yasser <yjeafar@gmail.com>
2020-06-13 00:20:08 -0400

    Create start menu and board in bash

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,25 +1,50 @@
-import tkinter as tk
+board =    [['', '', ''],  # Define board as two dimensional array. Can make this scalable to any size with a bit or rework
+            ['', '', ''],
+            ['', '', '']]
 
-# class Application(tk.Frame):
-#     def __init__(self, master=None):
-#         super().__init__(master)
-#         self.master = master
-#         self.pack()
-#         self.create_widgets()
 
-#     def create_widgets(self):
-#         self.hi_there = tk.Button(self)
-#         self.hi_there["text"] = "Hello World\n(click me)"
-#         self.hi_there["command"] = self.say_hi
-#         self.hi_there.pack(side="top")
+def drawBoard():
+    for i in range(len(board)):
+        print(board[i][0] + '  |' + board[i][1] + '   |' + board[i][2]) # Draw board dynamically
+        try:
+            if (board[i + 1]):
+                print('---------')
+        except IndexError: # Except to trying to read null
+            pass
 
-#         self.quit = tk.Button(self, text="QUIT", fg="red",
-#                               command=self.master.destroy)
-#         self.quit.pack(side="bottom")
+def startPlayerVsPlayer():
+    # start message here
+    drawBoard()
 
-#     def say_hi(self):
-#         print("hi there, everyone!")
 
-# root = tk.Tk()
-# app = Application(master=root)
-# app.mainloop()
+
+def startPlayerVsComputer():
+    # start message here
+    drawBoard()
+
+
+
+
+def main():
+    optionSelection = ''
+    while optionSelection not in [1, 2, 3]:
+        optionSelection = int(input('Please select one:\n 1. Enter 1 to go against a player \n 2. Enter 2 to go against the computer \n 3. Enter 3 to Exit \n'))
+        if (optionSelection == 1):
+            selection = getPlayerSelection()
+            startPlayerVsPlayer()
+        elif(optionSelection == 2):
+            selection = getPlayerSelection()
+            startPlayerVsComputer()
+        elif(optionSelection == 3):
+            print('Farewell!')
+        else:
+            print ('Value is invalid. Please try again.')
+
+def getPlayerSelection():
+    playerSelection = input('First turn is selected randomly. Would you like to be X or O? ').capitalize()
+    while playerSelection not in ['O','X']:
+        playerSelection = input('Input was invalid. Please try again. ').capitalize()
+    return playerSelection
+
+
+main()
