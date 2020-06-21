0e464651ffc610cfa2024011c807a4ec2bbfc7f9
Yasser <yjeafar@gmail.com>
2020-06-21 17:07:17 -0400

    added examples in PythonTutorial file in root folder

--- Python3Tutorial.py
+++ Python3Tutorial.py
@@ -1,8 +1,27 @@
-print('hi') # ctrl + b to run
+board =  [['1', '1', '2'],
+          ['2', '1', '2'],
+          ['2', '2', '1']]
 
-programming = 'Python', 'Java', 'C', 'C#'
+# diag = []
 
-print(type(programming)) # Prints the type of variable
+# for index in range(len(board)):
+#     diag.append(board[index][index])
+# for i in diag:
+#     print(i)
 
-for language in programming:
-    print(language)
+boardLength = range(len(board))
+diagonalValues = zip(boardLength, reversed((boardLength)))
+
+for i, j in diagonalValues:
+    print(i, j)
+    
+print()
+
+for i, j in enumerate(reversed(boardLength)): # Same thing as loop above but looks much cleaner this way and simpler
+    print(i, j)
+
+
+
+# Alt + Shift + up or down to add cursor
+# (0,0), (1,1), (2,2) or (0,2), (1,1), (2,0)
+
